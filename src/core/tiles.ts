c2c69bcbed6d21d401858353542e861e2e3c1628
conu0w0 <h010185@smsh.ntpc.edu.tw>
2026-01-20 02:21:55 +0800

    Add Tile class and TileValue type

--- src/core/tiles.ts
+++ src/core/tiles.ts
@@ -1,1 +1,13 @@
+export type TileValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
+export class Tile {
+  value: TileValue;
+
+  constructor(value: TileValue) {
+    this.value = value;
+  }
+
+  toString() {
+    return this.value.toString();
+  }
+}
