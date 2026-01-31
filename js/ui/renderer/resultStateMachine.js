f7e52b0661377d7b2e4c0505cad19d3b3621a293
conu0w0 <h010185@smsh.ntpc.edu.tw>
2026-02-01 05:05:38 +0800

    Create resultStateMachine.js

---
+++ js/ui/renderer/resultStateMachine.js
@@ -0,0 +1,27 @@
+export const RESULT_STATE = {
+    INIT: 0,
+    TITLE: 1,
+    WINNER: 2,
+    YAKU_ANIM: 3,
+    YAKU_STATIC: 4,
+    HAND: 5,
+    SCORE: 6,
+    LEVEL: 7,
+    HINT: 8
+};
+
+export class ResultStateMachine {
+    constructor() {
+        this.state = RESULT_STATE.INIT;
+        this.stateEnterTime = 0;
+    }
+
+    enter(state) {
+        this.state = state;
+        this.stateEnterTime = performance.now();
+    }
+
+    timeInState() {
+        return performance.now() - this.stateEnterTime;
+    }
+}
